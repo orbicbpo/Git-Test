6131a8006ab29be56ed14b2f9afdd97775e726c0
Abhishek <abhishek50001@gmail.com>
2018-07-30 13:05:48 -0500

    index.php & bootswatch.css

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+<link rel="stylesheet" type="text/css" href="bootswatch.css" />
+<head>
+	<title></title>
+</head>
+<body>
+Helllo
+</body>
+</html>
